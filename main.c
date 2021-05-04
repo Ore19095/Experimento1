bfd13a62e7cc8c35aca959312e97f30e1b8c72d8
Ore19095 <68124151+Ore19095@users.noreply.github.com>
2021-05-03 18:56:04 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/sysctl.h"
+
+
+/**
+ * main.c
+ */
+int main(void){
+    // PARTE 2: CONFIGURACION DEL RELOJ
+    // configuración del reloj, uso de pll, con divisor de 5, la frecuencia es de 40 MHz
+    SysCtlClockSet(SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_SYSDIV_5 | SYSCTL_OSC_MAIN);
+
+
+
+
+
+	return 0;
+}
