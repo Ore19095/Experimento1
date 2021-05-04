2831f28e308c455585ff9bce7f023a28236117fd
Ore19095 <68124151+Ore19095@users.noreply.github.com>
2021-05-04 14:23:19 -0600

    Update main.c

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 int main(void){
     // PARTE 2: CONFIGURACION DEL RELOJ
     // configuración del reloj, uso de pll, con divisor de 5, la frecuencia es de 40 MHz
-    SysCtlClockSet(SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_SYSDIV_8 | SYSCTL_OSC_MAIN);
-
+    SysCtlClockSet(SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_SYSDIV_4 | SYSCTL_OSC_MAIN);
+    // PARTE 3: configurar los pines como salida
 
 
 
